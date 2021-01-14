fd534094868457ca994eb59eac775d227b795c14
stupidk <deadintegral@gmail.com>
2021-01-14 16:20:47 +0900

    feat: apply css in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
-import Link from 'next/link'
 import Layout from 'components/Layout'
 import ArticleList from 'components/article/ArticleList'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
+  <Layout title="Nextjs TS Realworld">
     <h1>Next TS Realworld</h1>
-    <div>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-      <ArticleList />
+    <div className="banner">banner</div>
+    <div className="home-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-9">
+            <ArticleList />
+          </div>
+          <div className="col-md-3">sidebar</div>
+        </div>
+      </div>
     </div>
   </Layout>
 )
