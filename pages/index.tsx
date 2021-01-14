bcb39c90bdbe80cf0062a75c880ea1dd6209814d
stupidk <deadintegral@gmail.com>
2021-01-14 18:45:43 +0900

    feat: add Banner.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import Layout from 'components/Layout'
 import ArticleList from 'components/article/ArticleList'
+import Banner from 'components/home/Banner'
 
 const IndexPage = () => (
   <Layout title="Nextjs TS Realworld">
     <h1>Next TS Realworld</h1>
-    <div className="banner">banner</div>
     <div className="home-page">
+      <Banner />
       <div className="container page">
         <div className="row">
           <div className="col-md-9">
--- utils/constant.ts
+++ utils/constant.ts
@@ -1,1 +1,3 @@
 export const BASE_URL = 'https://conduit.productionready.io/api'
+
+export const APP_NAME = `conduit`;
