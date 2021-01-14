1b63057ce04f290b2c65d776f3f498e129197535
stupidk <deadintegral@gmail.com>
2021-01-14 21:06:31 +0900

    feat: header extract from index and insert app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,18 @@
 import ArticleList from 'components/article/ArticleList'
 import Banner from 'components/home/Banner'
-import Header from 'components/common/Header'
 
 const IndexPage = () => (
-  <>
-    <Header />
-    <div className="home-page">
-      <Banner />
-      <div className="container page">
-        <div className="row">
-          <div className="col-md-9">
-            <ArticleList />
-          </div>
-          <div className="col-md-3">sidebar</div>
+  <div className="home-page">
+    <Banner />
+    <div className="container page">
+      <div className="row">
+        <div className="col-md-9">
+          <ArticleList />
         </div>
+        <div className="col-md-3">sidebar</div>
       </div>
     </div>
-  </>
+  </div>
 )
 
 export default IndexPage
