b0686c2476a473fdab8300a555ee1b0c4b79f516
stupidk <deadintegral@gmail.com>
2021-01-14 16:22:01 +0900

    feat: articleList apply css and add author

--- components/article/ArticlePreview.tsx
+++ components/article/ArticlePreview.tsx
@@ -5,13 +5,27 @@ interface ArticlePreviewProps {
   article: articleReadType
 }
 const ArticlePreview = ({ article }: ArticlePreviewProps) => {
-	return <div>
-		<CustomLink href="/article/[pid]" as={`/article/${article.slug}`}>
-			<h1>{article.title}</h1>
-			<p>{article.description}</p>
-			<span>Read more...</span>
-		</CustomLink>
-	</div>
+  const { author } = article
+  return (
+    <div className="article-preview">
+      <div className="article-meta">
+        <CustomLink href="/profile/[pid]" as={`/profile/${author.username}`}>
+          <img src={author.image} />
+        </CustomLink>
+        <CustomLink href="/profile/[pid]" as={`/profile/${author.username}`}>
+          <div className="info">
+            <span className="author">{author.username}</span>
+            <span className="date">{new Date(article.createdAt).toDateString()}</span>
+          </div>
+        </CustomLink>
+      </div>
+      <CustomLink href="/article/[pid]" as={`/article/${article.slug}`} className="preview-link">
+        <h1>{article.title}</h1>
+        <p>{article.description}</p>
+        <span>Read more...</span>
+      </CustomLink>
+    </div>
+  )
 }
 
 export default ArticlePreview
