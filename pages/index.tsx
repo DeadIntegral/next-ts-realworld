d57ffcee5ec43daadbcd9247a4dc00ba6575f5ab
stupidk <deadintegral@gmail.com>
2021-01-14 19:48:41 +0900

    feat: delete origin Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
-import Layout from 'components/Layout'
 import ArticleList from 'components/article/ArticleList'
 import Banner from 'components/home/Banner'
 
 const IndexPage = () => (
-  <Layout title="Nextjs TS Realworld">
-    <h1>Next TS Realworld</h1>
+  <>
     <div className="home-page">
       <Banner />
       <div className="container page">
@@ -16,7 +14,7 @@ const IndexPage = () => (
         </div>
       </div>
     </div>
-  </Layout>
+  </>
 )
 
 export default IndexPage
