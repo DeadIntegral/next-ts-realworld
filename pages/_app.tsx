ea3167e4326170cf3ad798b7b1fbf3cbd9beece8
stupidk <deadintegral@gmail.com>
2021-01-16 23:44:32 +0900

    fix: add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
 import type { AppProps } from 'next/app'
 import Header from 'components/common/Header'
+import Head from 'next/head'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <Head>
+        <title>Conduit</title>
+      </Head>
       <Header />
       <Component {...pageProps} />
     </>
