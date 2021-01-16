981abf3850cff77f05b495dd0053369091062142
stupidk <deadintegral@gmail.com>
2021-01-16 18:37:21 +0900

    feat: add register api in auth.ts

--- api/auth.ts
+++ api/auth.ts
@@ -5,6 +5,10 @@ const AuthAPI = {
     requests.post('/users/login', {
       user: { email, password },
     }),
+  register: async (username: string, email: string, password: string) =>
+    requests.post('/users', {
+      user: { username, email, password },
+    }),
 }
 
 export default AuthAPI
