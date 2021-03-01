f75de2d5713101cbec9e5652eec8bd97b4373167
stupidk <deadintegral@gmail.com>
2021-03-01 20:55:29 +0900

    feat: add articleForm and api

--- api/article.ts
+++ api/article.ts
@@ -1,9 +1,11 @@
 import { requests } from './wrapperAPI'
+import { articleWriteType } from 'types/ArticleTypes'
 
 const limitQuery = (limit: number, page: number) => `limit=${limit}&offset=${page ? page * limit : 0}`
 const ArticleAPI = {
   all: (page: number, limit: number = 10) => requests.get(`/articles?${limitQuery(limit, page)}`),
   get: (slug: string) => requests.get(`/articles/${encodeURIComponent(slug)}`),
+  create: (article: articleWriteType) => requests.post(`/articles`, { ...article }),
 }
 
 export default ArticleAPI
